80d706055cb65cbc879b7f8e122f13f97ef5cdf4
Mariel Schlaefer <mschlaefer00@gmail.com>
2017-11-21 16:10:44 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,48 @@
+var a = 10
+var b = 5
+var n = 564
+
+function add(a, b) {
+  return (a + b)
+}
+
+function subtract(a, b) {
+  return (a - b)
+}
+
+function multiply(a, b) {
+  return (a * b)
+}
+
+function divide(a, b) {
+  return (a / b)
+}
+
+function inc(n) {
+  return (n += 1)
+}
+
+function dec(n) {
+  return (n -= 1)
+}
+
+function makeInt(n) {
+  parseInt(n, 10)
+}
+
+function makeInt(n) {
+  parseInt('0')
+}
+
+function makeInt(n) {
+  parseInt('sldkjflksjf')
+}
+
+function makeInt(n) {
+  parseFloat('2.222')
+}
+
+function preserveDecimal(n) {
+  parseFloat('sldkjflksjf')
+}
+
